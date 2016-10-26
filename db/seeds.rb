45705f2f13e722f903bd33a33981b97219cfb18c
PaulBryan2007 <PaulBryan2007@gmail.com>
2016-10-26 16:50:30 +0100

    ticket model working

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,4 +14,4 @@
 #
 u1 = User.create!(username: "kenny", first_name: "Kenny", last_name: "Loggins", email: "kenny@kenny.com", password: "password", password_confirmation: "password")
 
-t1 = Ticket.create!(club: "Spurs", image: "http://i1.mirror.co.uk/incoming/article1478778.ece/ALTERNATES/s615b/Spurs%20v%20West%20Ham%20ticket", value: 100_000, family_enclosure: false)
+t1 = Ticket.create!(club: "Arsenal", image: "http://i1.mirror.co.uk/incoming/article1478778.ece/ALTERNATES/s615b/Spurs%20v%20West%20Ham%20ticket", value: 100_000, family_enclosure: false)
